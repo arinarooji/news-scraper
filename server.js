ac56aa0c5cdb118e94aa381b5b0831dfa42108a0
Keivon Arooji <karooji0@email.cpcc.edu>
2018-01-09 12:46:50 -0500

    setup connection

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Declare dependencies
+//DEPENDENCIES
 var express    = require("express");
 var bodyParser = require("body-parser");
 var cheerio    = require("cheerio");  // Parses our HTML and helps us find elements
@@ -6,7 +6,7 @@ var request    = require("request");  // Makes HTTP request for HTML page
 var mongoose   = require("mongoose"); // Our newest addition to the dependency family
 
 const config = require("./config").init();
-var Example  = require("./exampleModel.js");
+var model    = require("./model.js");
 let port     = require("./config").port;
 ////////////////////////////////////////
 
@@ -16,15 +16,14 @@ var app = express(); // Initialize Express
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(express.static("public"));
 //Mongoose
-mongoose.Promise = Promise;   //Leverage ES6 promises, mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
-var db = mongoose.connection; // Save our mongoose connection to db
+mongoose.Promise = Promise; //Leverage ES6 promises, mongoose
+var db = mongoose.connect(config.db.uri, { useMongoClient: true }); //mLab connection
 
-// If there's a mongoose error, log it to console
+//Log mongoose errors
 db.on("error", function(error) {
     console.log("Mongoose Error: ", error);
 });
-// Once we "open" a connection to mongoose, tell the console we're in
+//Log connection success
 db.once("open", function() {
     console.log("Mongoose connection successful.");
 });
