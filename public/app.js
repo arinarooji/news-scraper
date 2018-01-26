194b8d6e29963fb80eefa9abbd61d2d4804af26c
Keivon Arooji <karooji0@email.cpcc.edu>
2018-01-26 18:56:02 -0500

    Post comment click event listener

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-console.log("Client-side JavaScript");
+$(document).ready(() => {
+    console.log("Client-side JavaScript");
+
+    //Post comment click event listener
+    $('.comment-btn').on('click', (event) => {
+        event.preventDefault();
+        let $btn = event.target;
+
+        console.log($($btn).data('id'));
+
+        //AJAX put request
+        //...
+
+    });
+
+
+});
